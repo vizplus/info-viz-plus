0b0704f6da2a706ed763d26eb04c717aa11a5c73
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2020-03-31 14:48:55 +0400

    Add shares scripts, struct for sql and crontab preset.

---
+++ share/restart_irreversible_waterfall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PIDFILE="/var/www/vizplus/info.viz.plus/module/waterfall_irreversible.pid"
+if test -f "$PIDFILE"; then
+	PID=`cat $PIDFILE`
+	echo "PIDFILE exist $PID"
+	PIDSTATUS=`ps -aux | grep [w]aterfall_irreversible.php | awk '{print $2}' | grep -v '^${PID}$'`
+	if [ "$PID" == "$PIDSTATUS" ]; then
+		echo "PID exist $PID, terminate file and wait..."
+		rm -rf "$PIDFILE"
+		MAXSLEEP=15000
+		SLEEPSTEP=10
+		while [ $MAXSLEEP -gt 0 ]
+		do
+			echo "Waiting ${MAXSLEEP}ms..."
+			sleep 0.1s
+			MAXSLEEP=$[ $MAXSLEEP - $SLEEPSTEP ]
+			PIDSTATUS=`ps -aux | grep [w]aterfall_irreversible.php | awk '{print $2}' | grep -v '^${PID}$'`
+			if [ "" == "$PIDSTATUS" ]
+			then
+				echo "PIDSTATUS empty, restarting!"
+				break
+			else
+				echo "PIDSTATUS=$PIDSTATUS, waiting empty"
+			fi
+		done
+	else
+		echo "PID not exist, restart"
+	fi
+else
+	echo "PIDFILE not exist, restart"
+fi
+echo "Starting new PID..."
+php /var/www/vizplus/info.viz.plus/module/waterfall_irreversible.php > ~/logs/waterfall_irreversible.log &
---
+++ share/restart_ops_working.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+PIDFILE="/var/www/vizplus/info.viz.plus/module/ops_working.pid"
+if test -f "$PIDFILE"; then
+	PID=`cat $PIDFILE`
+	echo "PIDFILE exist $PID"
+	PIDSTATUS=`ps -aux | grep [o]ps_working.php | awk '{print $2}' | grep -v '^${PID}$'`
+	echo "======="
+	echo "PIDSTATUS equal $PIDSTATUS"
+	if [ "$PID" == "$PIDSTATUS" ]; then
+		echo "equal!"
+	else
+		echo "not equal!"
+	fi
+	if [ "$PID" == "$PIDSTATUS" ]; then
+		echo "PID exist $PID, terminate file and wait..."
+		rm -rf "$PIDFILE"
+		MAXSLEEP=15000
+		SLEEPSTEP=10
+		while [ $MAXSLEEP -gt 0 ]
+		do
+			echo "Waiting ${MAXSLEEP}ms..."
+			sleep 0.1s
+			MAXSLEEP=$[ $MAXSLEEP - $SLEEPSTEP ]
+			PIDSTATUS=`ps -aux | grep [o]ps_working.php | awk '{print $2}' | grep -v '^${PID}$'`
+			if [ "" == "$PIDSTATUS" ]; then
+				echo "PIDSTATUS empty, restarting!"
+				break
+			else
+				echo "PIDSTATUS=$PIDSTATUS, waiting empty"
+			fi
+		done
+	else
+		echo "PID not exist, restart"
+	fi
+else
+	echo "PIDFILE not exist, restart"
+fi
+echo "Starting new PID..."
+php /var/www/vizplus/info.viz.plus/module/ops_working.php > ~/logs/ops_working.log &
