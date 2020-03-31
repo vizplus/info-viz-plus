fb714c5b767cd7f870ab5f5aa3e767aca2339029
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2020-03-31 14:44:56 +0400

    Third party js and css. Moded sortable with double click/touch event fix, pretty-print-json with accounts&witnesses links support.

---
+++ js/pretty-print-json.js
@@ -0,0 +1,86 @@
+//! pretty-print-json v0.2.1 ~ github.com/center-key/pretty-print-json ~ MIT License
+const prettyPrintJson = {
+   version: '0.2.1',
+   toHtml(thing, options) {
+      const op_type_witnesses_keys={
+      	'witness_reward':['witness'],
+      };
+      const op_type_accounts_keys={
+      	'award':['initiator','receiver'],
+      	'benefactor_award':['initiator','benefactor'],
+      	'receive_award':['initiator','receiver'],
+      	'account_metadata':['account'],
+      	'transfer':['from','to'],
+      };
+      const defaults = { indent: 4, quoteKeys: false, escapeHtml: false, type:false };
+      const settings = Object.assign(defaults, options);
+      const htmlEntities = (string) => {
+         // Makes text displayable in browsers
+         return string
+            .replace(/&/g,   '&amp;')
+            .replace(/\\"/g, '&bsol;&quot;')
+            .replace(/</g,   '&lt;')
+            .replace(/>/g,   '&gt;');
+         };
+      const buildValueHtml = (value,key,op_type) => {
+         // Returns a string like: "<span class=json-number>3.1415</span>"
+         const strType =  /^"/.test(value) && 'string';
+         const boolType = ['true', 'false'].includes(value) && 'boolean';
+         const nullType = value === 'null' && 'null';
+         const type =     boolType || nullType || strType || 'number';
+			if(-1!=Object.keys(op_type_witnesses_keys).indexOf(op_type)){
+				if(-1!=op_type_witnesses_keys[op_type].indexOf(key)){
+					let buf=value;
+					buf=buf.replace(/^\"+/g,'');
+					buf=buf.replace(/\"+$/g,'');
+					value='"<a class="view-account" href="/witnesses/'+buf+'/">'+buf+'</a>"';
+				}
+			}
+			if(-1!=Object.keys(op_type_accounts_keys).indexOf(op_type)){
+				if(-1!=op_type_accounts_keys[op_type].indexOf(key)){
+					let buf=value;
+					buf=buf.replace(/^\"+/g,'');
+					buf=buf.replace(/\"+$/g,'');
+					value='"<a class="view-account" href="/accounts/'+buf+'/">'+buf+'</a>"';
+				}
+			}
+         return '<span class=json-' + type + '>' + value + '</span>';
+         };
+      const replacer = (match, p1, p2, p3, p4) => {
+         // Converts the four parenthesized capture groups (indent, key, value, end) into HTML
+         const part =       { indent: p1, key: p2, value: p3, end: p4 };
+         console.log(part);
+         const findName =   settings.quoteKeys ? /(.*)(): / : /"([\w]+)": |(.*): /;
+         const indentHtml = part.indent || '';
+         const keyName =    part.key && part.key.replace(findName, '$1$2');
+         const keyHtml =    part.key ? '<span class=json-key>' + keyName + '</span>: ' : '';
+         const valueHtml =  part.value ? buildValueHtml(part.value,keyName,settings.type) : '';
+         const endHtml =    part.end || '';
+         return indentHtml + keyHtml + valueHtml + endHtml;
+         };
+      const jsonLine = /^( *)("[^"]+": )?("[^"]*"|[\w.+-]*)?([{}[\],]*)?$/mg;
+         // Regex parses each line of the JSON string into four parts:
+         //    Capture group       Part        Description                  '   "active": true,'
+         //    ------------------  ----------  ---------------------------  --------------------
+         //    ( *)                p1: indent  Spaces for indentation       '   '
+         //    ("[^"]+": )         p2: key     Key name                     '"active": '
+         //    ("[^"]*"|[\w.+-]*)  p3: value   Key value                    'true'
+         //    ([{}[\],]*)         p4: end     Line termination characters  ','
+      var json = JSON.stringify(thing, null, settings.indent);
+      if(settings.escapeHtml){
+          return htmlEntities(json).replace(jsonLine, replacer);
+      }
+      else{
+      	  json=json.split('\\"').join('%%_REPLACER_%%');
+      	  json=json.replace(jsonLine, replacer);
+      	  json=json.split('%%_REPLACER_%%').join('\\"');
+          return json;
+      }
+      }
+
+   };
+
+if (typeof module === 'object')
+   module.exports = prettyPrintJson;  //node module loading system (CommonJS)
+if (typeof window === 'object')
+   window.prettyPrintJson = prettyPrintJson;  //support both global and window property
---
+++ templates/index.tpl
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>{title}</title>
+	<meta name="description" content="{description}">
+	<meta property="og:description" content="{description}">
+	<meta name="twitter:description" content="{description}">
+	<meta name="viewport" content="width=device-width">
+	{head_addon}
+
+	<link rel="stylesheet" href="/app.css?{css_change_time}">
+	<script type="text/javascript" src="/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="/app.js?{script_change_time}"></script>
+
+	<script type="text/javascript" src="/js/highcharts.js"></script>
+	<script type="text/javascript" src="/js/highcharts-exporting.js"></script>
+	<link rel="stylesheet" href="/css/sortable-theme-slick.css">
+	<script type="text/javascript" src="/js/sortable.js"></script>
+	<script type="text/javascript" src="/js/pretty-print-json.js"></script>
+</head>
+<body>
+<div class="header shadow unselectable">
+	<div class="horizontal-view">
+		<div class="logo"><a href="/" class="prefix{index_page_selected}">info.</a><a href="https://viz.plus/"><img src="/logo.svg" alt="VIZ+"></a></div>
+		<div class="menu-list captions" style="display: block;">
+			<div class="menu-bg">
+				{menu}
+			</div>
+		</div>
+	</div>
+</div>
+<div class="horizontal-view vertical-view">
+	{content}
+</div>
+</body>
+</html>
